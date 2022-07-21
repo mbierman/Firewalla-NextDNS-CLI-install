a44b1a29ddf354e7e83fae74bf592f948892bb9a
Michael Bierman <mbierman@users.noreply.github.com>
2022-07-20 22:25:53 -0700

    Update pihole_docker_install.sh

--- pihole_docker_install.sh
+++ pihole_docker_install.sh
@@ -11,28 +11,31 @@ if [ ! -d "$path2" ]; then
         mkdir $path2
 fi
 
-curl https://raw.githubusercontent.com/mbierman/unifi-installer/main/docker-compose.yaml > $path2/docker-compose.yaml
+curl https://github.com/mbierman/pihole-installer-for-Firewalla/blob/main/docker-compose.yaml > $path2/docker-compose.yaml
  
 
 cd $path2
+sudo systemctl start docker
+sudo docker-compose pull
+sudo docker-compose up --no-start
+sudo ip route add 172.16.0.0/24 dev br-$(sudo docker network inspect pi-hole_default |jq -r '.[0].Id[0:12]') table lan_routable
+sudo ip route add 172.16.0.0/24 dev br-$(sudo docker network inspect pi-hole_default |jq -r '.[0].Id[0:12]') table wan_routable
+sudo docker-compose up --detach
 
-sudo systemctl start docker-compose@unifi
 
 sudo docker ps
 
 echo -n "Starting docker (this can take ~ one minute)"
-while [ -z "$(sudo docker ps | grep unifi | grep -o Up)" ]
+while [ -z "$(sudo docker ps | grep pihole | grep -o Up)" ]
 do
         echo -n "."
         sleep 2s
 done
 echo "Done"
 
-sudo ip route add 172.16.1.0/24 dev br-$(sudo docker network ls | awk '$2 == "unifi_default" {print $1}') table lan_routable
-sudo ip route add 172.16.1.0/24 dev br-$(sudo docker network ls | awk '$2 == "unifi_default" {print $1}') table wan_routable
-echo address=/unifi/172.16.1.2 > ~/.firewalla/config/dnsmasq_local/unifi
+echo address=/pihole/172.16.0.2 > ~/.firewalla/config/dnsmasq_local/pihole
 sudo systemctl restart firerouter_dns
-sudo docker restart unifi
+sudo docker restart pihole
 
 path3=/home/pi/.firewalla/config/post_main.d
 if [ ! -d "$path3" ]; then
@@ -41,19 +44,19 @@ fi
 
 echo "#!/bin/bash
 sudo systemctl start docker
-sudo systemctl start docker-compose@unifi
 sudo ipset create -! docker_lan_routable_net_set hash:net
-sudo ipset add -! docker_lan_routable_net_set 172.16.1.0/24
+sudo ipset add -! docker_lan_routable_net_set 172.16.0.0/24
 sudo ipset create -! docker_wan_routable_net_set hash:net
-sudo ipset add -! docker_wan_routable_net_set 172.16.1.0/24" >  /home/pi/.firewalla/config/post_main.d/start_unifi.sh
+sudo ipset add -! docker_wan_routable_net_set 172.16.0.0/24
+sudo systemctl start docker-compose@pi-hole" >  /home/pi/.firewalla/config/post_main.d/start_pihole.sh
 
-chmod a+x /home/pi/.firewalla/config/post_main.d/start_unifi.sh
+chmod a+x /home/pi/.firewalla/config/post_main.d/start_pihole.sh
 
 echo -n "Restarting docker"
-sudo docker start unifi
-while [ -z "$(sudo docker ps | grep unifi | grep Up)" ]
+sudo docker start pihole
+while [ -z "$(sudo docker ps | grep pihole | grep Up)" ]
 do
         echo -n "."
         sleep 2s
 done
-echo -e "Done!\n\nYou can open https://172.16.1.2:8443 in your favorite browser and set up your UniFi Controller now. (\n\nNote it may not have a certificate so the browser may give you a security warning.)\n\n"
+echo -e "Done!\n\nYou can open  http://172.16.0.2 in your favorite browser and set up your pi-hole now. (\n\nNote it may not have a certificate so the browser may give you a security warning.)\n\n"
