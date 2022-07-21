318a21f07457bc792dba9c776d47ea76f7c4c5ef
Michael Bierman <mbierman@users.noreply.github.com>
2022-07-21 13:34:24 -0700

    Update uninstall_pihole.sh

--- uninstall_pihole.sh
+++ uninstall_pihole.sh
@@ -2,6 +2,7 @@
 
 sudo docker container stop pihole && sudo docker container rm pihole
 # sudo rm -rf /data/pi-hole # use cautiously! this will peranently remove settings.
+echo -e "You may also want to run \`sudo rm -rf /data/pi-hole\` but that will remove all pi-hole settings so be cautious."
 rm /home/pi/.firewalla/run/docker/pi-hole/docker-compose.yaml
 sudo systemctl stop docker-compose@pi-hole
 rm /home/pi/.firewalla/config/post_main.d/start_pihole.sh
