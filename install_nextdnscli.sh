5447637778b890bce938b39022863da15b4fe209
Michael Bierman <mbierman@users.noreply.github.com>
2022-09-08 10:37:48 -0700

    Update install_nextdnscli.sh

--- install_nextdnscli.sh
+++ install_nextdnscli.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
-  
+
 # install & configure NextDNS CLI on startup of Firewalla
 # file goes in: /home/pi/.firewalla/config/post_main.d/
 # DNS over HTTPS must be disabled in Firewalla app
-# Based on Brian Curtis' script https://help.firewalla.com/hc/en-us/community/posts/7469669689619-NextDNS-CLI-on-Firewalla-revisited-working-DHCP-host-resolution-in-NextDNS-logs-
+
+
+install=/home/pi/.firewalla/config/post_main.d/install_nextdnscli.sh 
+if [ ! -f "$install" ] ; then
+        curl https://raw.githubusercontent.com/mbierman/Firewalla-NextDNS-CLI-install/main/install_nextdnscli.sh > $install
+        echo install saved.
+else
+        echo install in place. 
+fi
+
+uninstall=/home/pi/.firewalla/config/post_main.d/uninstall_nextdnscli.nosh 
+if [ ! -f "$install" ] ; then
+        curl https://raw.githubusercontent.com/mbierman/Firewalla-NextDNS-CLI-install/main/uninstall_nextdns_CLI.nosh > $install
+        echo uninstall saved.
+else
+        echo uninstall in place
+fi
+
+# replace -config 123456 with your own NextDNS config ID
+# replace 10.10.12.1 with your Firewalla local lan IP
+id=123456
+IP=10.10.12.1
+
+if [[ -z $id ]] ; then
+        echo -n "Your nextdns ID is not set.\n You should edit $install before running" 
+        exit
+elif [[ -z $IP ]] ; then
+        echo -n "Your Firewalla IP is not set.\n You should edit $install before running" 
+        exit
+else
+        echo -n "fully configured and ready to go!"
+fi
+exit
 
 # install NextDNS CLI
 sudo wget -qO /usr/share/keyrings/nextdns.gpg https://repo.nextdns.io/nextdns.gpg
@@ -12,14 +44,11 @@ unalias apt
 sudo apt update
 sudo apt install nextdns
 
-# replace -config 123456 with your own NextDNS config ID
-# replace 10.10.12.1 with your Firewalla local lan IP
+
 # enable NextDNS caching: https://github.com/nextdns/nextdns/wiki/Cache-Configuration
 # set discovery-dns to IP of Firewalla local DNS
 # set NextDNS CLI to listen on local network IP (instead of 127.0.0.1 -- allows DHCP host resolution in NextDNS logs)
 # define listen port instead of relying on -setup-router
-id=123456
-IP=10.10.12.1
 sudo nextdns install -config $id -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns $IP -listen ${IP}:5555
 
 # alternate command to implement conditional configuration: https://github.com/nextdns/nextdns/wiki/Conditional-Configuration
