ba6be6a3e2f56565fcec5e8a22ce93248209bcb1
Michael Bierman <mbierman@users.noreply.github.com>
2022-10-14 15:28:57 -0700

    Update install_nextdnscli.sh

--- install_nextdnscli.sh
+++ install_nextdnscli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 2.0.5
+# 2.10
 # Based on a script by Brian Curtis 
 # https://help.firewalla.com/hc/en-us/community/posts/7469669689619-NextDNS-CLI-on-Firewalla-revisited-working-DHCP-host-resolution-in-NextDNS-logs-
 
@@ -8,97 +8,26 @@
 # file goes in: /home/pi/.firewalla/config/post_main.d/
 # DNS over HTTPS must be disabled in Firewalla app
 
-if [ -f /data/stopnextdns ] ; then
-        echo "❌  No nextDNS" 
-        exit
-fi
-
-
 # set id with your own NextDNS config ID
 # set IP with your Firewalla local lan IP
 id=
 IP=
+# These are optional. It is currently set up to use the same VPNID for WireGuard and OpenVPN. 
 VPNID=
+# Put your OpenVPN and WireGuard IP ranges here. 
 OpenVPN=
 WireGuard=
-appletvid=
 
-# check for configuration
-if [[ -z $id ]] ; then
-        echo -e "Your nextdns ID is not set.\nEdit $install and run again."
+
+if [ -f /data/stopnextdns ] ; then
+        echo "❌  No nextDNS" 
         exit
-elif [[ -z $IP ]] ; then
-        echo -e "Your Firewalla IP is not set.\nEdit $uninstall and run again."
-        exit
-else
-        echo -e "Fully configured and ready to go!\n\n"
+fi
+DIR="/home/pi/.firewalla/config/post_main.d"
+if [ ! -d  $DIR ]; then
+	mkdir /home/pi/.firewalla/config/post_main.d 
 fi
 
-# Install script if not installed. 
-install=/home/pi/.firewalla/config/post_main.d/install_nextdnscli.sh 
-if [ ! -f "$install" ] ; then
-        curl https://raw.githubusercontent.com/mbierman/Firewalla-NextDNS-CLI-install/main/install_nextdnscli.sh > $install
-        chmod +x $install
-        echo "✅  install saved."
-else
-        echo "✅  install in place. "
-fi
-
-# Install Uninstall script if not installed
-uninstall=/home/pi/.firewalla/config/post_main.d/uninstall_nextdnscli.nosh 
-if [ ! -f "$uninstall" ] ; then
-        curl https://raw.githubusercontent.com/mbierman/Firewalla-NextDNS-CLI-install/main/uninstall_nextdns_cli.nosh > $uninstall
-        chmod +x $uninstall
-        echo "✅  uninstall saved."
-else
-        echo "✅  uninstall in place.."
-fi
-
-# install NextDNS CLI
-if [ -z "$(command -v nextdns)" ] ; then 
-	sudo wget -qO /usr/share/keyrings/nextdns.gpg https://repo.nextdns.io/nextdns.gpg
-	echo "deb [signed-by=/usr/share/keyrings/nextdns.gpg] https://repo.nextdns.io/deb stable main" | sudo tee /etc/apt/sources.list.d/nextdns.list
-	unalias apt
-	sudo apt update
-	sudo apt install nextdns
-else
-	echo "✅  nextdns already installed..."
-fi
-
-# enable NextDNS caching: https://github.com/nextdns/nextdns/wiki/Cache-Configuration
-# set discovery-dns to IP of Firewalla local DNS
-# set NextDNS CLI to listen on local network IP (instead of 127.0.0.1 -- allows DHCP host resolution in NextDNS logs)
-# define listen port instead of relying on -setup-router
-# sudo nextdns install -config $id -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns $IP/24 -listen ${IP}:5555
-
-sudo nextdns install \
--config ${IP}/24=${id} \
--config $OpenVPN/24=$VPNID \
--config $WireGuard/24=$VPNID \
--config -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns ${IP} -listen ${IP}:5555 
-# You can also put a config for an individual mac address like so. 
-# -config xx:yy:zz:aa:bb:cc=${id} \
-
-
-sudo nextdns restart
-
-# sudo nextdns install -config $id -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns $IP -listen ${IP}:5555
-
-# alternate command to implement conditional configuration: https://github.com/nextdns/nextdns/wiki/Conditional-Configuration
-# sudo nextdns install -config $IP/24=abcdef -config 123456 -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns 10.10.12.1 -listen 10.10.12.1:5555
-
-# Add dnsmasq integration to enable client reporting in NextDNS logs: https://github.com/nextdns/nextdns/wiki/DNSMasq-Integration
-
-cat > /home/pi/.firewalla/config/dnsmasq/mynextdns.conf << EOF
-server=${IP}#5555
-add-mac
-add-subnet=32,128
-EOF
-
-# restart Firewalla DNS service
-sudo systemctl restart firerouter_dns.service
-
-echo "nextdns is... $(sudo nextdns status)"
 
 # Install validation Script if not installed. 
 nextdnstest=/data/nextdnstest.sh
@@ -119,3 +48,83 @@ if [ ! -f "$nextdnsdata" ] ; then
 else
         echo "✅  data in place."
 fi
+
+
+# check for configuration
+if [[ -z $id ]] ; then
+        echo -e "Your nextdns ID is not set.\nEdit $install and run again."
+        exit
+elif [[ -z $IP ]] ; then
+        echo -e "Your Firewalla IP is not set.\nEdit $uninstall and run again."
+        exit
+else
+        echo -e "Fully configured and ready to go!\n\n"
+fi
+
+# Install script if not installed. 
+install=/home/pi/.firewalla/config/post_main.d/install_nextdnscli.sh 
+if [ ! -f "$install" ] ; then
+	sudo apt install ca-certificates
+        curl https://raw.githubusercontent.com/mbierman/Firewalla-NextDNS-CLI-install/main/install_nextdnscli.sh > $install
+        chmod +x $install
+        echo "✅  install saved."
+else
+        echo "✅  install in place. "
+fi
+
+# Install Uninstall script if not installed
+uninstall=/home/pi/.firewalla/config/post_main.d/uninstall_nextdnscli.nosh 
+if [ ! -f "$uninstall" ] ; then
+        curl https://raw.githubusercontent.com/mbierman/Firewalla-NextDNS-CLI-install/main/uninstall_nextdns_cli.nosh > $uninstall
+        chmod +x $uninstall
+        echo "✅  uninstall saved."
+else
+        echo "✅  uninstall in place.."
+fi
+# install NextDNS CLI
+if [ -z "$(command -v nextdns)" ] ; then 
+	sudo wget -qO /usr/share/keyrings/nextdns.gpg https://repo.nextdns.io/nextdns.gpg
+	echo "deb [signed-by=/usr/share/keyrings/nextdns.gpg] https://repo.nextdns.io/deb stable main" | sudo tee /etc/apt/sources.list.d/nextdns.list
+	unalias apt
+	sudo apt update
+	sudo apt install nextdns
+else
+	echo "✅  nextdns already installed..."
+fi
+
+cat > /home/pi/.firewalla/config/dnsmasq/mynextdns.conf << EOF
+server=${IP}#5555
+add-mac
+add-subnet=32,128
+EOF
+
+echo -e "\nStarting nextdns...\n\n"
+
+sudo nextdns install \
+-config ${IP}/24=${id} \
+-report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns ${IP} -listen ${IP}:5555 
+
+# enable NextDNS caching: https://github.com/nextdns/nextdns/wiki/Cache-Configuration
+# set discovery-dns to IP of Firewalla local DNS
+# set NextDNS CLI to listen on local network IP (instead of 127.0.0.1 -- allows DHCP host resolution in NextDNS logs)
+# define listen port instead of relying on -setup-router
+# sudo nextdns install -config $id -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns $IP/24 -listen ${IP}:5555
+
+# You can also put a config for an individual mac address like so. Edit to include your actual mac addres and
+# put thse before the config above.
+# -config xx:yy:zz:aa:bb:cc=${id} \
+
+# you can use nextdns on OpenVPN or WireGuard Put thse before the config above.
+# -config $OpenVPN/24=$VPNID \
+# -config $WireGuard/24=$VPNID \
+
+# sudo nextdns install -config $id -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns $IP -listen ${IP}:5555
+
+# alternate command to implement conditional configuration: https://github.com/nextdns/nextdns/wiki/Conditional-Configuration
+# sudo nextdns install -config $IP/24=abcdef -config 123456 -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns 10.10.12.1 -listen 10.10.12.1:5555
+
+# Add dnsmasq integration to enable client reporting in NextDNS logs: https://github.com/nextdns/nextdns/wiki/DNSMasq-Integration
+
+# sudo nextdns restart 
+echo "Restarting Firewalla DNS..." 
+sudo systemctl restart firerouter_dns.service && echo "nextdns is... $(sudo nextdns status)"
