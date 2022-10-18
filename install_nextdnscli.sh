e2c97b2ad4250e97e55dea3ff9bba8aa80445974
Michael Bierman <mbierman@users.noreply.github.com>
2022-10-17 19:40:15 -0700

    Update install_nextdnscli.sh
    Bugfix

--- install_nextdnscli.sh
+++ install_nextdnscli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 2.10
+# 2.1.1
 # Based on a script by Brian Curtis 
 # https://help.firewalla.com/hc/en-us/community/posts/7469669689619-NextDNS-CLI-on-Firewalla-revisited-working-DHCP-host-resolution-in-NextDNS-logs-
 
@@ -10,24 +10,24 @@
 
 # set id with your own NextDNS config ID
 # set IP with your Firewalla local lan IP
-id=
-IP=
-# These are optional. It is currently set up to use the same VPNID for WireGuard and OpenVPN. 
-VPNID=
-# Put your OpenVPN and WireGuard IP ranges here. 
-OpenVPN=
-WireGuard=
+id=42eadb
+IP=192.168.0.1
+VPNID=f1273e
+OpenVPN=10.128.98.1
+WireGuard=10.189.55.1
+appletvid=7c6a43
 
+install=/home/pi/.firewalla/config/post_main.d/install_nextdnscli.sh 
 
 if [ -f /data/stopnextdns ] ; then
         echo "❌  No nextDNS" 
         exit
 fi
+
 DIR="/home/pi/.firewalla/config/post_main.d"
 if [ ! -d  $DIR ]; then
 	mkdir /home/pi/.firewalla/config/post_main.d 
 fi
-
 
 # Install validation Script if not installed. 
 nextdnstest=/data/nextdnstest.sh
@@ -49,6 +49,7 @@ else
         echo "✅  data in place."
 fi
 
+
 # check for configuration
 if [[ -z $id ]] ; then
         echo -e "Your nextdns ID is not set.\nEdit using your favorite editor (vi is already installed on Firewalla) run $install and run."
@@ -60,22 +61,8 @@ else
         echo -e "Fully configured and ready to go!\n\n"
 fi
 
-
-# check for configuration
-if [[ -z $id ]] ; then
-        echo -e "Your nextdns ID is not set.\nEdit $install and run again."
-        exit
-elif [[ -z $IP ]] ; then
-        echo -e "Your Firewalla IP is not set.\nEdit $uninstall and run again."
-        exit
-else
-        echo -e "Fully configured and ready to go!\n\n"
-fi
-
 # Install script if not installed. 
-install=/home/pi/.firewalla/config/post_main.d/install_nextdnscli.sh 
 if [ ! -f "$install" ] ; then
-	sudo apt install ca-certificates
         curl https://raw.githubusercontent.com/mbierman/Firewalla-NextDNS-CLI-install/main/install_nextdnscli.sh > $install
         chmod +x $install
         echo "✅  install saved."
@@ -94,6 +81,7 @@ else
 fi
 # install NextDNS CLI
 if [ -z "$(command -v nextdns)" ] ; then 
+	sudo apt install ca-certificates
 	sudo wget -qO /usr/share/keyrings/nextdns.gpg https://repo.nextdns.io/nextdns.gpg
 	echo "deb [signed-by=/usr/share/keyrings/nextdns.gpg] https://repo.nextdns.io/deb stable main" | sudo tee /etc/apt/sources.list.d/nextdns.list
 	unalias apt
@@ -112,7 +100,12 @@ EOF
 echo -e "\nStarting nextdns...\n\n"
 
 sudo nextdns install \
+-config C8:D0:83:E3:2C:21=$appletvid \
+-config C8:D0:83:D7:7A:6E=$appletvid \
+-config C8:D0:83:DD:09:2C=$appletvid \
 -config ${IP}/24=${id} \
+-config $OpenVPN/24=$VPNID \
+-config $WireGuard/24=$VPNID \
 -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns ${IP} -listen ${IP}:5555 
 
 # enable NextDNS caching: https://github.com/nextdns/nextdns/wiki/Cache-Configuration
@@ -120,19 +113,9 @@ sudo nextdns install \
 # set NextDNS CLI to listen on local network IP (instead of 127.0.0.1 -- allows DHCP host resolution in NextDNS logs)
 # define listen port instead of relying on -setup-router
 # sudo nextdns install -config $id -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns $IP/24 -listen ${IP}:5555
-
-# You can also put a config for an individual mac address like so. Edit to include your actual mac addres and
-# put thse before the config above.
+# You can also put a config for an individual mac address like so. 
 # -config xx:yy:zz:aa:bb:cc=${id} \
 
-# you can use nextdns on OpenVPN or WireGuard Put thse before the config above.
-# -config $OpenVPN/24=$VPNID \
-# -config $WireGuard/24=$VPNID \
-
-# sudo nextdns install -config $id -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns $IP -listen ${IP}:5555
-
-# alternate command to implement conditional configuration: https://github.com/nextdns/nextdns/wiki/Conditional-Configuration
-# sudo nextdns install -config $IP/24=abcdef -config 123456 -report-client-info -cache-size=10MB -max-ttl=5s -discovery-dns 10.10.12.1 -listen 10.10.12.1:5555
 
 # Add dnsmasq integration to enable client reporting in NextDNS logs: https://github.com/nextdns/nextdns/wiki/DNSMasq-Integration
 
