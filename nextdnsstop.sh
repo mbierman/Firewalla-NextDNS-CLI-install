245538270f1681231d716cab01b6d0c5cfec9b0b
Michael Bierman <mbierman@users.noreply.github.com>
2023-07-20 11:00:53 -0700

    Update nextdnsstop.sh
    Enhance stopping nextdns

--- nextdnsstop.sh
+++ nextdnsstop.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+period=4
 
 echo  "stopping nextdns..."
-sudo nextdns deactivate
-sudo nextdns stop
-
+sudo nextdns deactivate && sleep $period
+sudo nextdns stop && sleep $period
 sudo nextdns status
+
 touch /data/stopnextdns
 echo created /data/stopnextdns remove this file to restart nextdns
+/data/flushdns.sh
