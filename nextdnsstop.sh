257a329898114ad02faea0ce87b10712a4ad1e2f
Michael Bierman <mbierman@users.noreply.github.com>
2025-07-12 16:28:26 -0700

    Update nextdnsstop.sh

--- nextdnsstop.sh
+++ nextdnsstop.sh
@@ -4,8 +4,16 @@ period=4
 echo  "stopping nextdns..."
 sudo nextdns deactivate && sleep $period
 sudo nextdns stop && sleep $period
-sudo nextdns status
+
+cat > $dnsmasq << EOF
+# server=${IP}#5555
+# add-mac
+# add-subnet=32,128
+EOF
+
+sudo systemctl stop firerouter_dns && sleep 5 && ssudo systemctl start firerouter_dns
 
 touch /data/stopnextdns
 echo created /data/stopnextdns remove this file to restart nextdns
 /data/flushdns.sh
+sudo nextdns status
